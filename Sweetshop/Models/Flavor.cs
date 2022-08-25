3b154628e862d9db9da0195c74a52b4cda036798
Louis Knolle <knollelw@gmail.com>
2022-08-25 14:32:33 -0400

    complete initial models

---
+++ Sweetshop/Models/Flavor.cs
@@ -0,0 +1,20 @@
+
+
+using System.Collections.Generic;
+
+namespace SweetShop.Models
+{
+  public class Flavor
+  {
+    public Flavor()
+    {
+      this.JoinEntities = new HashSet<FlavorTreat>();
+    }
+
+    public int FlavorId { get; set; }
+    public string Name { get; set; }
+    public virtual ApplicationUser User { get; set; }
+
+    public virtual ICollection<FlavorTreat> JoinEntities { get; set; }
+  }
+}
---
+++ Sweetshop/Models/SweetShopContext.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace SweetShop.Models
+{
+  public class SweetShopContext : IdentityDbContext<ApplicationUser>
+  {
+    public DbSet<Flavor> Flavors { get; set; }
+    public DbSet<Treat> Treats { get; set; }
+    public DbSet<FlavorTreat> Flavor { get; set; }
+
+    public SweetShopContext(DbContextOptions options) : base(options) { }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      optionsBuilder.UseLazyLoadingProxies();
+    }
+  }
+}
