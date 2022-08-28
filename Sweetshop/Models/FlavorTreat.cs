7f38046e1f76a6157045106c3730094a455dfe3b
Louie Knolle <91914975+louieknolle@users.noreply.github.com>
2022-08-27 23:42:18 -0400

    fix last typos

--- Sweetshop/Models/FlavorTreat.cs
+++ Sweetshop/Models/FlavorTreat.cs
@@ -4,17 +4,11 @@ namespace SweetShop.Models
 {
   public class FlavorTreat
   { 
-    // public FlavorTreat()
-    // {
-    //   this.JoinEntities = new HashSet<FlavorTreat>();
-    // }
 
     public int FlavorTreatId { get; set; }
     public int TreatId { get; set; }
     public int FlavorId { get; set; }
     public virtual Treat Treat { get; set; }
     public virtual Flavor Flavor { get; set; }
-
-    // public virtual ICollection<FlavorTreat> JoinEntities { get; set; }
   }
 }
